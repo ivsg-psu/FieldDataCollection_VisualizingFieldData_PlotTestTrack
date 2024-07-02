668337c9978e6b275254781154caa48139e08e83
Abel Kim <spk5951@psu.edu>
2024-07-02 19:59:59 -0400

    FEAT: Added functions that could take LLA and ENU coordinates to a circle and rectangle
    The function and script for rangeRSU_circle were added. The function and script for rangeRSU_rectangle were added.

---
+++ Functions/fcn_plotTestTrack_rangeRSU_rectangle.m
@@ -0,0 +1,46 @@
+function plotTestTrack_rangeRSU_rectangle(reference_latitude, reference_longitude, reference_altitude, rsu_coordinates_enu, width, length)
+    reference_latitude = 40.86368573;
+    reference_longitude = -77.83592832;
+    reference_altitude = 344.189;
+    gps_object = GPS(reference_latitude, reference_longitude, reference_altitude);
+
+    lla_coords = gps_object.ENU2WGSLLA(rsu_coordinates_enu);
+
+    half_width = width / 2;
+    half_length = length / 2;
+    rectangle_enu = [
+        rsu_coordinates_enu(1) - half_width, rsu_coordinates_enu(2) - half_length;
+        rsu_coordinates_enu(1) + half_width, rsu_coordinates_enu(2) - half_length;
+        rsu_coordinates_enu(1) + half_width, rsu_coordinates_enu(2) + half_length;
+        rsu_coordinates_enu(1) - half_width, rsu_coordinates_enu(2) + half_length;
+        rsu_coordinates_enu(1) - half_width, rsu_coordinates_enu(2) - half_length;
+    ];
+
+    rectangle_lla = gps_object.ENU2WGSLLA(rectangle_enu);
+
+    figure;
+    h_geoplot = geoplot(lla_coords(1), lla_coords(2), '*','Color',[0 1 0],'LineWidth',3,'MarkerSize',10);
+    h_parent = get(h_geoplot, 'Parent');
+    set(h_parent, 'ZoomLevel', 16.375);
+    try
+        geobasemap satellite;
+    catch
+        geobasemap openstreetmap;
+    end
+    hold on;
+
+    geoplot(rectangle_lla(:,1), rectangle_lla(:,2), 'Color', [0 0 1], 'LineWidth', 1.5);
+
+    hold off;
+
+    figure;
+    plot(rsu_coordinates_enu(1), rsu_coordinates_enu(2), '*', 'Color', [0 1 0], 'LineWidth', 3, 'MarkerSize', 10);
+    hold on;
+
+    plot(rectangle_enu(:,1), rectangle_enu(:,2), 'Color', [0 0 1], 'LineWidth', 1.5);
+
+    hold off;
+    xlabel('East (m)');
+    ylabel('North (m)');
+    axis equal;
+end
---
+++ Functions/script_test_fcn_PlotTestTrack_rangeRSU_circle.m
@@ -0,0 +1,75 @@
+%% Basic example 1: 20ft vs 30ft
+
+close all;
+csv_filenames = {'20ft.csv', '30ft.csv'};
+
+optional_parameters = {
+    {[1, 0, 0], 20, [-41.54388324160499, -111.0086546354483, -12.592103434894112], 1, 200}, 
+    {[0, 0, 1], 10, [29.746349250935626, 124.74785860585521, -9.039292080071405], 1, 200}   
+};
+
+fcn_PlotTestTrack_rangeRSU_circle(csv_filenames, optional_parameters);
+
+%% Basic example 2: 30ft vs 50ft
+
+close all;
+csv_filenames = {'30ft.csv', '50ft.csv'};
+
+optional_parameters = {
+    {[1, 0, 0], 20, [-41.54388324160499, -111.0086546354483, -12.592103434894112], 1, 200}, 
+    {[0, 0, 1], 10, [29.746349250935626, 124.74785860585521, -9.039292080071405], 1, 200}   
+};
+
+fcn_PlotTestTrack_rangeRSU_circle(csv_filenames, optional_parameters);
+
+%% Pittsburg Test Site
+
+close all;
+csv_filenames = {'PittsburgTestStartMiddlePart1.csv', 'PittsburgTestStartMiddlePart2.csv'};
+
+RSU_Location1 = load("pole_coordinates.mat","pole8_coordinates_enu");
+RSU_Location2 = load("pole_coordinates.mat","pole9_coordinates_enu");
+
+
+optional_parameters = {
+    {[1, 1, 0], 10, RSU_Location1.pole8_coordinates_enu, 1, 1000}, 
+    {[0, 1, 1], 10, RSU_Location2.pole9_coordinates_enu, 1, 1000}   
+};
+
+fcn_PlotTestTrack_rangeRSU_circle(csv_filenames, optional_parameters);
+
+legend("BSM plot at RSU 1","RSU1 location","BSM plot at RSU2","RSU2locatiobn");
+
+%% Pittsburg Test Site Middle Part 1
+
+close all;
+csv_filenames = {'PittsburgTestStartMiddlePart1.csv'};
+
+RSU_Location1 = load("pole_coordinates.mat","pole8_coordinates_enu");
+
+
+
+optional_parameters = {
+    {[1, 1, 0], 10, RSU_Location1.pole8_coordinates_enu, 1, 1000}, 
+  
+};
+
+fcn_PlotTestTrack_rangeRSU_circle(csv_filenames, optional_parameters);
+
+legend("BSM plot at RSU 1","RSU1 location");
+
+%% Pittsburg Test Site Middle Part 2
+
+close all;
+csv_filenames = {'PittsburgTestStartMiddlePart2.csv'};
+
+RSU_Location2 = load("pole_coordinates.mat","pole9_coordinates_enu");
+
+
+optional_parameters = {
+    {[0, 1, 1], 10, RSU_Location2.pole9_coordinates_enu, 1, 1000}   
+};
+
+fcn_PlotTestTrack_rangeRSU_circle(csv_filenames, optional_parameters);
+
+legend("BSM plot at RSU2","RSU2 location");
