9e05c1f8f8a0169e607dfda8d5b0e13e36ea8a9d
Jiabao <jpz5469@psu.edu>
2024-07-07 23:13:10 -0400

    FEAT: Amination Trace Line of car with boundary line respect to time

---
+++ Functions/script_test_fcn_PlotTestTrack_LineWithBoundaryWithTime.m
@@ -0,0 +1,10 @@
+% script_test_fcn_PlotTestTrack_LineWithBoundaryWithTime
+% This is a script to exercise the function:
+% fcn_PlotTestTrack_LineWithBoundaryWithTime.m 
+% This function was written on 2024_07_07 by Jiabao Zhao, jpz5469@psu.edu
+
+
+%% test 1
+csvFile = '50ft.csv'; % Path to your CSV file
+fcn_PlotTestTrack_LineWithBoundaryWithTime('50ft.csv', [], [], true); 
+
